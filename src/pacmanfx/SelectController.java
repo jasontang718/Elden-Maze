10aaf783e7ea247661ef3ac8b21ed51312ce5c68
jason <jasontang@localhost>
2024-06-30 22:35:05 +0800

    Merge origin/main into main

--- src/pacmanfx/SelectController.java
+++ src/pacmanfx/SelectController.java
@@ -1,6 +1,5 @@
 package pacmanfx;
 
-import java.io.IOException;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,20 +8,23 @@ import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class SelectController implements Initializable {
 
     @FXML
     private Text title;
+     
+   
+    private Model model; // Reference to the model
 
+    public void select(Model model) {
+        this.model = model;
+    }
+ 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
         // Create the floating animation
         TranslateTransition transition = new TranslateTransition(Duration.seconds(2), title);
         transition.setByY(-10);  // Move up by 10 pixels
@@ -30,17 +32,14 @@ public class SelectController implements Initializable {
         transition.setCycleCount(TranslateTransition.INDEFINITE);
         transition.play();
     }
-    
-    @FXML
-    private void handleNextScene() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("next.fxml"));
-            Parent nextRoot = loader.load();
-            Scene nextScene = new Scene(nextRoot);
-            Stage stage = (Stage) title.getScene().getWindow();
-            stage.setScene(nextScene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+     @FXML
+  
+    public void startGame() {
+        Scene gameScene = model.getGameScene();
+        model.setScene(gameScene);
+        
     }
+
+    
 }
+
